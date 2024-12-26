55b8d27c668055efce8b336302477546706624ce
InfinitGamer <jeremycomino@gmail.com>
2024-12-26 10:27:27 +0100

    removed comments

--- UI/automaticBakeUI/ObjectsPanelAuto.py
+++ UI/automaticBakeUI/ObjectsPanelAuto.py
@@ -12,15 +12,15 @@ class ObjectsPanelAuto(bpy.types.Panel):
 
     def draw(self, context):
         layout = self.layout
-        # cogemos la escena actual
+        
         scene = context.scene
 
-        # cogemos los settings del autobake
+        
         autobake_settings = scene.autobake_settings
 
-        # cogemos los elementos para la UI de bake
+        
         UIbake_settings = scene.UIbake_settings
-        # creamos lista de objetos
+        
         column_object_list = layout.column()
 
         column_object_list.template_list(
@@ -31,6 +31,6 @@ class ObjectsPanelAuto(bpy.types.Panel):
             UIbake_settings,
             "object_index",
         )
-        # botones para añadir y eliminar objetos
+        
         column_object_list.operator("ui.add_object", text="Add Selected Objects")
         column_object_list.operator("ui.delete_object", text="Delete Selected Object")
